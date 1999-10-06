8fd06cb4ecc93a42ef25cf7095efb308ab024f45
Peder Blekken <pederb@sim.no>
1999-10-06 10:28:35 +0000

    *** empty log message ***

--- src/entities/Polyline.cpp
+++ src/entities/Polyline.cpp
@@ -7,8 +7,7 @@
  *
  *  This library is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License, version 2, as
- *  published by the Free Software Foundation.  DO NOT MISTAKE THIS LICENSE
- *  FOR THE GNU LGPL LICENSE.
+ *  published by the Free Software Foundation.
  *
  *  This library is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,14 +15,10 @@
  *  General Public License (the accompanying file named COPYING) for more
  *  details.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
  **************************************************************************
  *
- *  If you need DIME for commercial purposes, you can contact Systems In
- *  Motion about acquiring a commercial license.
+ *  If you need DIME for a non-GPL project, contact Systems In Motion
+ *  to acquire a Professional Edition License:
  *
  *  Systems In Motion                                   http://www.sim.no/
  *  Prof. Brochs gate 6                                       sales@sim.no
