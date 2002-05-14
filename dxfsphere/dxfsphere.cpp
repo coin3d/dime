a390b77c16af367fcfa8b203dee60352553ae67c
Peder Blekken <pederb@sim.no>
2002-05-14 10:01:13 +0000

    New example program. bootstrap.

---
+++ dxfsphere/dxfsphere.cpp
@@ -0,0 +1,373 @@
+/*
+ * sphere - generate a triangle mesh approximating a sphere by
+ *  recursive subdivision. First approximation is an platonic
+ *  solid; each level of refinement increases the number of
+ *  triangles by a factor of 4.
+ *
+ * Level 3 (128 triangles for an octahedron) is a good tradeoff if
+ *  gouraud shading is used to render the database.
+ *
+ * Usage: sphere [level] [-p] [-c] [-f] [-t] [-i]
+ *      level is an integer >= 1 setting the recursion level (default 1).
+ *      -c causes triangles to be generated with vertices in counterclockwise
+ *          order as viewed from the outside in a RHS coordinate system.
+ *          The default is clockwise order.
+ *      -t starts with a tetrahedron instead of an octahedron
+ *      -i starts with a icosahedron instead of an octahedron
+ *
+ *  The subroutines print_object() and print_triangle() should
+ *  be changed to generate whatever the desired database format is.
+ *
+ * Jon Leech (leech @ cs.unc.edu) 3/24/89
+ * icosahedral code added by Jim Buddenhagen (jb1556@daditz.sbc.com) 5/93
+ *
+ * C++ version and Dime output added by pederb@sim.no
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+#include <dime/Model.h>
+#include <dime/sections/EntitiesSection.h>
+#include <dime/entities/3DFace.h>
+#include <dime/Output.h>
+#include <dime/util/Linear.h>
+
+class point {
+public:
+  point() { }
+  point(const point & p) 
+    : x(p.x), y(p.y), z(p.z) { }
+  point(double x, double y, double z)
+    : x(x), y(y), z(z) { }
+public:
+  double  x, y, z;
+};
+
+class triangle {
+public:
+  triangle(void) { }
+  triangle(const point & p0, const point & p1, const point & p2,
+           double area) {
+    pt[0] = p0;
+    pt[1] = p1;
+    pt[2] = p2;
+    area = area;
+  } 
+public:
+  point     pt[3];    /* Vertices of triangle */
+  double    area;     /* Unused; might be used for adaptive subdivision */
+};
+
+class object {
+public:
+  object(void) : npoly(0), poly(NULL) { }
+  object(int npoly, triangle * poly) 
+    : npoly(npoly), poly(poly) { }
+public:
+  int       npoly;    /* # of triangles in object */
+  triangle *poly;     /* Triangles */
+};
+
+/* Six equidistant points lying on the unit sphere */
+#define XPLUS point(1,  0,  0)    /*  X */
+#define XMIN  point(-1,  0,  0)    /* -X */
+#define YPLUS point(0,  1,  0)    /*  Y */
+#define YMIN  point(0, -1,  0)    /* -Y */
+#define ZPLUS point(0,  0,  1)    /*  Z */
+#define ZMIN  point(0,  0, -1)    /* -Z */
+
+/* Vertices of a unit octahedron */
+triangle octahedron[] = {
+  triangle(XPLUS, ZPLUS, YPLUS, 0.0),
+  triangle(YPLUS, ZPLUS, XMIN, 0.0),
+  triangle(XMIN , ZPLUS, YMIN, 0.0),
+  triangle(YMIN , ZPLUS, XPLUS, 0.0),
+  triangle(XPLUS, YPLUS, ZMIN, 0.0),
+  triangle(YPLUS, XMIN , ZMIN, 0.0),
+  triangle(XMIN , YMIN , ZMIN, 0.0),
+  triangle(YMIN , XPLUS, ZMIN, 0.0)
+  };
+
+/* A unit octahedron */
+object oct(sizeof(octahedron) / sizeof(octahedron[0]),
+  octahedron);
+
+/* Vertices of a tetrahedron */
+#define sqrt_3 0.5773502692
+#define PPP point(sqrt_3,  sqrt_3,  sqrt_3)   /* +X, +Y, +Z */
+#define MMP point(-sqrt_3, -sqrt_3,  sqrt_3)   /* -X, -Y, +Z */
+#define MPM point(-sqrt_3,  sqrt_3, -sqrt_3)   /* -X, +Y, -Z */
+#define PMM point(sqrt_3, -sqrt_3, -sqrt_3)   /* +X, -Y, -Z */
+
+/* Structure describing a tetrahedron */
+triangle tetrahedron[] = {
+  triangle(PPP, MMP, MPM, 0.0),
+  triangle(PPP, PMM, MMP, 0.0),
+  triangle(MPM, MMP, PMM, 0.0),
+  triangle(PMM, PPP, MPM, 0.0)
+  };
+
+object tet(
+sizeof(tetrahedron) / sizeof(tetrahedron[0]),
+  tetrahedron);
+
+/* Twelve vertices of icosahedron on unit sphere */
+#define tau 0.8506508084      /* t=(1+sqrt(5))/2, tau=t/sqrt(1+t^2)  */
+#define one 0.5257311121      /* one=1/sqrt(1+t^2) , unit sphere     */
+#define ZA point(tau,  one,    0)
+#define ZB point(-tau,  one,    0)
+#define ZC point(-tau, -one,    0)
+#define ZD point( tau, -one,    0)
+#define YA point( one,   0 ,  tau)
+#define YB point( one,   0 , -tau)
+#define YC point(-one,   0 , -tau)
+#define YD point(-one,   0 ,  tau)
+#define XA point(  0 ,  tau,  one)
+#define XB point(  0 , -tau,  one)
+#define XC point(  0 , -tau, -one)
+#define XD point(  0 ,  tau, -one)
+
+/* Structure for unit icosahedron */
+triangle icosahedron[] = {
+  triangle(YA, XA, YD, 0.0),
+  triangle(YA, YD, XB, 0.0),
+  triangle(YB, YC, XD, 0.0),
+  triangle(YB, XC, YC, 0.0),
+  triangle(ZA, YA, ZD, 0.0),
+  triangle(ZA, ZD, YB, 0.0),
+  triangle(ZC, YD, ZB, 0.0),
+  triangle(ZC, ZB, YC, 0.0),
+  triangle(XA, ZA, XD, 0.0),
+  triangle(XA, XD, ZB, 0.0),
+  triangle(XB, XC, ZD, 0.0),
+  triangle(XB, ZC, XC, 0.0),
+  triangle(XA, YA, ZA, 0.0),
+  triangle(XD, ZA, YB, 0.0),
+  triangle(YA, XB, ZD, 0.0),
+  triangle(YB, ZD, XC, 0.0),
+  triangle(YD, XA, ZB, 0.0),
+  triangle(YC, ZB, XD, 0.0),
+  triangle(YD, ZC, XB, 0.0),
+  triangle(YC, XC, ZC, 0.0)
+};
+
+/* unit icosahedron */
+object ico(sizeof(icosahedron) / sizeof(icosahedron[0]),
+  icosahedron);
+
+/* Forward declarations */
+point *normalize(point * p);
+point *midpoint(point * a, point * b);
+void flip_object(object * obj);
+void print_object(object * obj, int level, dimeModel & model);
+void print_triangle(triangle *t, dimeModel & model);
+
+int
+main(int ac, char ** av)
+{
+  object * old = &oct,         /* Default is octahedron */
+    * newobj;
+  int ccwflag = 1,        /* Reverse vertex order if true */
+    i,
+    level,              /* Current subdivision level */
+    maxlevel = 0;       /* Maximum subdivision level */
+
+  char * outfile = NULL;
+  
+  /* Parse arguments */
+  for (i = 1; i < ac; i++) {
+    if (!strcmp(av[i], "-c"))
+      ccwflag = 1;
+    else if (!strcmp(av[i], "-t"))
+      old = &tet;
+    else if (!strcmp(av[i], "-i"))
+      old = &ico;
+    else if (!strcmp(av[i], "-o") && i < ac-1) {
+      outfile = av[i+1];
+      i++;
+    }
+    else if (isdigit(av[i][0])) {
+      if ((maxlevel = atoi(av[i])) < 1) {
+	fprintf(stderr, "dxfsphere: # of levels must be >= 1\n");
+	exit(1);
+      }
+    } 
+    else {
+      break;
+    }
+  }
+  
+  if (i < ac || ac == 1) {
+    fprintf(stderr, "dxfsphere: [-c] [-t] [-i] [-o <outfile>] <levels>\n");
+    exit(1);
+  }
+
+  // initialize dime output
+  dimeOutput out;
+  if (!outfile || !out.setFilename(outfile)) {
+    out.setFileHandle(stdout);
+  }
+  dimeModel model;
+  dimeEntitiesSection * entities = new dimeEntitiesSection;
+  model.insertSection(entities);
+
+  if (ccwflag)
+    flip_object(old);
+  
+  /* Subdivide each starting triangle (maxlevel - 1) times */
+  for (level = 1; level < maxlevel; level++) {
+    /* Allocate a new object */
+    newobj = (object *)malloc(sizeof(object));
+    if (newobj == NULL) {
+      fprintf(stderr, "%s: Out of memory on subdivision level %d\n",
+              av[0], level);
+      exit(1);
+    }
+    newobj->npoly = old->npoly * 4;
+    
+    /* Allocate 4* the number of points in the current approximation */
+    newobj->poly  = (triangle *)malloc(newobj->npoly * sizeof(triangle));
+    if (newobj->poly == NULL) {
+      fprintf(stderr, "%s: Out of memory on subdivision level %d\n",
+              av[0], level);
+      exit(1);
+    }
+      
+    /* Subdivide each triangle in the old approximation and normalize
+     *  the new points thus generated to lie on the surface of the unit
+     *  sphere.
+     * Each input triangle with vertices labelled [0,1,2] as shown
+     *  below will be turned into four new triangles:
+     *
+     *                      Make new points
+     *                          a = (0+2)/2
+     *                          b = (0+1)/2
+     *                          c = (1+2)/2
+     *        1
+     *       /\             Normalize a, b, c
+     *      /  \
+     *    b/____\ c         Construct new triangles
+     *    /\    /\              [0,b,a]
+     *   /  \  /  \             [b,1,c]
+     *  /____\/____\            [a,b,c]
+     * 0      a     2           [a,c,2]
+     */
+    for (i = 0; i < old->npoly; i++) {
+      triangle
+        *oldt = &old->poly[i],
+        *newt = &newobj->poly[i*4];
+      point a, b, c;
+      
+      a = *normalize(midpoint(&oldt->pt[0], &oldt->pt[2]));
+      b = *normalize(midpoint(&oldt->pt[0], &oldt->pt[1]));
+      c = *normalize(midpoint(&oldt->pt[1], &oldt->pt[2]));
+        
+      newt->pt[0] = oldt->pt[0];
+      newt->pt[1] = b;
+      newt->pt[2] = a;
+      newt++;
+        
+      newt->pt[0] = b;
+      newt->pt[1] = oldt->pt[1];
+      newt->pt[2] = c;
+      newt++;
+      
+      newt->pt[0] = a;
+      newt->pt[1] = b;
+      newt->pt[2] = c;
+      newt++;
+      
+      newt->pt[0] = a;
+      newt->pt[1] = c;
+      newt->pt[2] = oldt->pt[2];
+    }
+    
+    if (level > 1) {
+      free(old->poly);
+      free(old);
+    }
+    
+    /* Continue subdividing new triangles */
+    old = newobj;
+  }
+  
+  /* Print out resulting approximation */
+  print_object(old, maxlevel, model);
+
+  model.write(&out);
+}
+
+/* Normalize a point p */
+point * normalize(point * p)
+{
+  static point r;
+  double mag;
+  
+  r = *p;
+  mag = r.x * r.x + r.y * r.y + r.z * r.z;
+  if (mag != 0.0) {
+    mag = 1.0 / sqrt(mag);
+    r.x *= mag;
+    r.y *= mag;
+    r.z *= mag;
+  }
+  
+  return &r;
+}
+
+/* Return the midpoint on the line between two points */
+point * midpoint(point * a, point * b)
+{
+  static point r;
+  
+  r.x = (a->x + b->x) * 0.5;
+  r.y = (a->y + b->y) * 0.5;
+  r.z = (a->z + b->z) * 0.5;
+  
+  return &r;
+}
+
+/* Reverse order of points in each triangle */
+void flip_object(object * obj)
+{
+  int i;
+  for (i = 0; i < obj->npoly; i++) {
+    point tmp;
+    tmp = obj->poly[i].pt[0];
+    obj->poly[i].pt[0] = obj->poly[i].pt[2];
+    obj->poly[i].pt[2] = tmp;
+  }
+}
+
+/* Write out all triangles in an object */
+void print_object(object * obj, int level, dimeModel & model)
+{
+  int i;
+  
+  for (i = 0; i < obj->npoly; i++) {
+    print_triangle(&obj->poly[i], model);  
+  }
+}
+
+
+/* Output a triangle */
+void print_triangle(triangle * t, dimeModel & model)
+{
+  int i;
+  
+  dime3DFace * face = new dime3DFace;
+  dimeVec3f v[3];
+
+  for (i = 0; i < 3; i++) {
+    v[i].x = t->pt[i].x;
+    v[i].y = t->pt[i].y;
+    v[i].z = t->pt[i].z;
+  }
+  face->setTriangle(v[0], v[1], v[2]);
+  model.addEntity(face);
+}
+
